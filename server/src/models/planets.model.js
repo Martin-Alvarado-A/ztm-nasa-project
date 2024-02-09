c96153b4341d1c87810caeb5d009a2dbbc00b54e
martin <martin.alvarado2002@gmail.com>
2024-02-09 14:04:59 +0100

    [9-112]: The Planets Model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,39 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  const planetConfirmed = planet['koi_disposition'] === 'CONFIRMED';
+  const solarFlux = planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11;
+  const planetRadii = planet['koi_prad'] < 1.6;
+
+  return planetConfirmed && solarFlux && planetRadii;
+};
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) habitablePlanets.push(data);
+  })
+  .on('error', (err) => {
+    console.log(`🔎 | Planets Model | error | ERROR:`, err);
+  })
+  .on('end', () => {
+    console.log(
+      `🔎 | Planets Model | end | habitablePlanets:`,
+      habitablePlanets.map((planet) => {
+        return planet['kepler_name'];
+      })
+    );
+    console.log(
+      `🔎 | Planets Model | end | ${habitablePlanets.length} habitable planets found!`
+    );
+  });
+
+module.exports = { planets: habitablePlanets };
